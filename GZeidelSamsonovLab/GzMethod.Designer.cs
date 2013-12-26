2008db84ba0c1ba1181972dfc2504bb370c3e67e
ishellstrike <ishellstrike@gmail.com>
2013-12-27 03:02:43 +0400

    initial

---
+++ GZeidelSamsonovLab/GzMethod.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GZeidelSamsonovLab
+{
+    partial class GzMethod
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.wrongSyntaxLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 63);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "((3;4;-4)(4;4;0)(-4;0;5))(3;4;3,1)";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input";
+            // 
+            // wrongSyntaxLabel
+            // 
+            this.wrongSyntaxLabel.AutoSize = true;
+            this.wrongSyntaxLabel.ForeColor = System.Drawing.Color.Red;
+            this.wrongSyntaxLabel.Location = new System.Drawing.Point(12, 94);
+            this.wrongSyntaxLabel.Name = "wrongSyntaxLabel";
+            this.wrongSyntaxLabel.Size = new System.Drawing.Size(69, 13);
+            this.wrongSyntaxLabel.TabIndex = 2;
+            this.wrongSyntaxLabel.Text = "wrong syntax";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(421, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // GzMethod
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 421);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.wrongSyntaxLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "GzMethod";
+            this.Text = "GzMethod";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label wrongSyntaxLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GZeidelSamsonovLab/GzMethod.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GZeidelSamsonovLab
+{
+    public partial class GzMethod : Form {
+        public const double Epsilon = 0.1;
+        public double[,] Input;
+        public double[] InputRight;
+
+        public GzMethod()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            wrongSyntaxLabel.Visible = false;
+            textBox1_TextChanged(null, null);
+        }
+
+        public bool GzEnding(double[] xk, double[] xkp) {
+            return !xk.Where((element, index) => Math.Abs(element - xkp[index]) >= Epsilon).Any();
+        }
+
+        public double[] GzMethodCalc(double[,] a, double[] b) {
+            var n = a.GetLength(0);
+            var x = new double[n];
+            var p = new double[n];
+            do
+            {
+                for (int i = 0; i < n; i++) {
+                    p[i] = x[i];
+                }
+
+                for (int i = 0; i < n; i++) {
+                    double var = 0;
+                    for (int j = 0; j < i; j++) {
+                        var += (a[i, j]*x[j]);
+                    }
+                    for (int j = i + 1; j < n; j++) {
+                        var += (a[i, j]*p[j]);
+                    }
+                    x[i] = (b[i] - var) / a[i, i];
+                }
+            }
+            while (!GzEnding(x, p));
+
+            return x;
+        }
+
+        //syntax ((1,2,3),(1,2,3),(1,2,3))(1,2,3)
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try {
+                string extracted = textBox1.Text;
+                var lastbr = extracted.LastIndexOf('('); 
+                string right = extracted.Substring(lastbr).Trim(' ');
+                string left = extracted.Substring(0, lastbr).Trim(' ');
+
+                var l = LeftParser(left);
+                var r = RightParser(right);
+                wrongSyntaxLabel.Visible = false;
+
+                label2.Text = ExtMatrixString(l, r);
+
+                var ountp = GzMethodCalc(l, r);
+
+                label3.Text = VectorString(ountp);
+            }
+            catch (Exception ex) {
+                wrongSyntaxLabel.Visible = true;
+            }
+        }
+
+        private static string VectorString(double[] l)
+        {
+            string s = "";
+            for (int i = 0; i < l.GetLength(0); i++)
+            {
+                    s += l[i] + "; ";
+                s += Environment.NewLine;
+            }
+            return s;
+        }
+
+        private static string ExtMatrixString(double[,] l, double[] r) {
+            string s = "";
+            for (int i = 0; i < l.GetLength(0); i++) {
+                for (int j = 0; j < l.GetLength(1); j++) {
+                    s += l[i, j] + "; ";
+                }
+                s += " | " + r[i];
+                s += Environment.NewLine;
+            }
+            return s;
+        }
+
+        private double[,] LeftParser(string s) {
+            var firstCol = new List<List<double>>();
+            bool afterFirst = false;
+            var parts = s.Split('(');
+
+
+            int y = 0;
+            foreach (var part in parts.Where(part => !string.IsNullOrEmpty(part))) {
+                if (!string.IsNullOrEmpty(part)) {
+                    var nums = part.Split(';').Select(t=>t.Trim(' ').Trim(')'));
+                    firstCol.Add(new List<double>());
+                    foreach (var num in nums.Where(num => !string.IsNullOrEmpty(num))) {
+                        firstCol[y].Add(Convert.ToDouble(num));
+                    }
+                    y++;
+                }
+            }
+            var o = new double[firstCol.Count, firstCol[0].Count];
+
+            for (int i = 0; i < firstCol.Count; i++) {
+                for (int j = 0; j < firstCol[0].Count; j++) {
+                    o[i, j] = firstCol[i][j];
+                }
+            }
+
+            return o;
+        }
+
+        private double[] RightParser(string s)
+        {
+            var firstCol = new List<double>();
+            var nums = s.Split(';').Select(t => t.Trim(' ').Trim(')').Trim('('));
+            foreach (var num in nums.Where(num => !string.IsNullOrEmpty(num)))
+            {
+                firstCol.Add(double.Parse(num));
+            }
+            var o = new double[firstCol.Count];
+
+            for (int i = 0; i < firstCol.Count; i++)
+            {
+                o[i] = firstCol[i];
+            }
+
+            return o;
+        }
+    }
+}
